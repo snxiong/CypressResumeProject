1813164a9a7c2beb9a2496218ea9bdd136072ebb
Steven Xiong <snxiong@hotmail.com>
2019-09-18 13:18:11 -0700

    Add files via upload

---
+++ login/Cypress Resume -Trial Password 1_files/animatedcollapseaddDiv.js
@@ -0,0 +1,342 @@
+
+	
+	
+	
+	/*collapsing divs on mainform.php*/
+	animatedcollapse.addDiv('date1', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date2', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date3', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date4', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date5', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date6', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date7', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date8', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date9', 'fade=1,height=40px')
+	animatedcollapse.addDiv('date10', 'fade=1,height=40px')
+	
+	animatedcollapse.addDiv('hidejobs', 'fade=0,hide=1')
+	animatedcollapse.addDiv('hidejobsmore', 'fade=1,hide=0')
+	
+	/*high school divs*/
+	animatedcollapse.addDiv('highschoolstatetext', 'fade=1,hide=1')
+	animatedcollapse.addDiv('highschoolstateselect', 'fade=1,hide=0')
+	animatedcollapse.addDiv('highschoolgrad', 'fade=1')
+	animatedcollapse.addDiv('honorshighschooldiv', 'fade=1,hide=1')
+	animatedcollapse.addDiv('graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('currentlyattending', 'fade=1')
+	
+	/*college divs*/
+	
+	/*divs that hide year drop downs*/
+	animatedcollapse.addDiv('collegegrad1', 'fade=1')
+	animatedcollapse.addDiv('collegegrad2', 'fade=1')
+	animatedcollapse.addDiv('collegegrad3', 'fade=1,')
+	animatedcollapse.addDiv('collegegrad4', 'fade=1')
+	animatedcollapse.addDiv('collegegrad5', 'fade=1')
+	
+	/*div that shows additional college entry areas*/
+	animatedcollapse.addDiv('hidecollege', 'fade=1,hide=1')
+	animatedcollapse.addDiv('hidecollegemore', 'fade=1,hide=0')
+	
+	/*divs that show and hide honors and currently attending inputs*/
+	animatedcollapse.addDiv('college1currentlyattending', 'fade=1')
+	animatedcollapse.addDiv('college2currentlyattending', 'fade=1')
+	animatedcollapse.addDiv('college3currentlyattending', 'fade=1')
+	animatedcollapse.addDiv('college4currentlyattending', 'fade=1')
+	animatedcollapse.addDiv('college5currentlyattending', 'fade=1')
+	animatedcollapse.addDiv('honorscollege1div', 'fade=1,hide=1')
+	animatedcollapse.addDiv('honorscollege2div', 'fade=1,hide=1')
+	animatedcollapse.addDiv('honorscollege3div', 'fade=1,hide=1')
+	animatedcollapse.addDiv('honorscollege4div', 'fade=1,hide=1')
+	animatedcollapse.addDiv('honorscollege5div', 'fade=1,hide=1')
+	animatedcollapse.addDiv('college1graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('college2graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('college3graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('college4graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('college5graduatedwithhonors', 'fade=1')
+	animatedcollapse.addDiv('degreecollege1div', 'fade=1')
+	animatedcollapse.addDiv('degreecollege2div', 'fade=1')
+	animatedcollapse.addDiv('degreecollege3div', 'fade=1')
+	animatedcollapse.addDiv('degreecollege4div', 'fade=1')
+	animatedcollapse.addDiv('degreecollege5div', 'fade=1')	
+	
+
+	/*certifications, training, and certificates*/
+	animatedcollapse.addDiv('certs', 'fade=1,hide=1')	
+	animatedcollapse.addDiv('certslink', 'fade=1,hide=0')	
+
+
+	
+	//specific statemens container divs for scrolling
+	animatedcollapse.addDiv('specificstatementcontainer1', 'speed=200,fade=0')
+	animatedcollapse.addDiv('specificstatementcontainer2', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer3', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer4', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer5', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer6', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('specificstatementcontainer7', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer8', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer9', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer10', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('specificstatementcontainer11', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer12', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer13', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer14', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer15', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer16', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer17', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer18', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer19', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('specificstatementcontainer20', 'speed=200,fade=0,hide=1')	
+
+
+	//tech statemens container divs
+	animatedcollapse.addDiv('techstatementcontainer1', 'speed=200,fade=0')
+	animatedcollapse.addDiv('techstatementcontainer2', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer3', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer4', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer5', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer6', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer7', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer8', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer9', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer10', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer11', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer12', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer13', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer14', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer15', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer16', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer17', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer18', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer19', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer20', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer21', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer22', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer23', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer24', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer25', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer26', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer27', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer28', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer29', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer30', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer31', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer32', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer33', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer34', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer35', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer36', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer37', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer38', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer39', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer40', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer41', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer42', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer43', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer44', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer45', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer46', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer47', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer48', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer49', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer50', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer51', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer52', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer53', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer54', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer55', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer56', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('techstatementcontainer57', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer58', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer59', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('techstatementcontainer60', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer61', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer62', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer63', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer64', 'speed=200,fade=0,hide=1')		
+	animatedcollapse.addDiv('techstatementcontainer65', 'speed=200,fade=0,hide=1')		
+
+	
+	//ability statemens container divs
+	animatedcollapse.addDiv('abilitystatementcontainer1', 'speed=200,fade=0')
+	animatedcollapse.addDiv('abilitystatementcontainer2', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer3', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer4', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer5', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer6', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('abilitystatementcontainer7', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer8', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer9', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer10', 'speed=200,fade=0,hide=1')	
+	animatedcollapse.addDiv('abilitystatementcontainer11', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer12', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer13', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer14', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer15', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer16', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer17', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer18', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer19', 'speed=200,fade=0,hide=1')
+	animatedcollapse.addDiv('abilitystatementcontainer20', 'speed=200,fade=0,hide=1')
+
+
+	/* div on get specific*/
+	animatedcollapse.addDiv('industry', 'fade=1,hide=1')
+	animatedcollapse.addDiv('hide', 'fade=1')
+	
+	animatedcollapse.addDiv('letters', 'fade=1,hide=1')
+	animatedcollapse.addDiv('lettershide', 'fade=1')	
+	
+	
+	/*divs on user_profile_creation_and_login.php*/
+	animatedcollapse.addDiv('profilesection3', 'fade=1,hide=1')	
+	
+	/*divs on publish.php*/
+	animatedcollapse.addDiv('colorchoice1', 'fade=1')
+	animatedcollapse.addDiv('colorchoice2', 'fade=1,hide=1')	
+	
+	/*divs on resume_manager.php/call_back_resumes.php*/
+	animatedcollapse.addDiv('1documentname1', 'fade=1')
+	animatedcollapse.addDiv('2documentname1', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname2', 'fade=1')
+	animatedcollapse.addDiv('2documentname2', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname3', 'fade=1')
+	animatedcollapse.addDiv('2documentname3', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname4', 'fade=1')
+	animatedcollapse.addDiv('2documentname4', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname5', 'fade=1')
+	animatedcollapse.addDiv('2documentname5', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname6', 'fade=1')
+	animatedcollapse.addDiv('2documentname6', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname7', 'fade=1')
+	animatedcollapse.addDiv('2documentname7', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname8', 'fade=1')
+	animatedcollapse.addDiv('2documentname8', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname9', 'fade=1')
+	animatedcollapse.addDiv('2documentname9', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname10', 'fade=1')
+	animatedcollapse.addDiv('2documentname10', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname11', 'fade=1')
+	animatedcollapse.addDiv('2documentname11', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname12', 'fade=1')
+	animatedcollapse.addDiv('2documentname12', 'fade=1,hide=1')			
+	
+	animatedcollapse.addDiv('1documentname13', 'fade=1')
+	animatedcollapse.addDiv('2documentname13', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname14', 'fade=1')
+	animatedcollapse.addDiv('2documentname14', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname15', 'fade=1')
+	animatedcollapse.addDiv('2documentname15', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname16', 'fade=1')
+	animatedcollapse.addDiv('2documentname16', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname17', 'fade=1')
+	animatedcollapse.addDiv('2documentname17', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname18', 'fade=1')
+	animatedcollapse.addDiv('2documentname18', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname19', 'fade=1')
+	animatedcollapse.addDiv('2documentname19', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname20', 'fade=1')
+	animatedcollapse.addDiv('2documentname20', 'fade=1,hide=1')		
+	
+	animatedcollapse.addDiv('1documentname21', 'fade=1')
+	animatedcollapse.addDiv('2documentname21', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname22', 'fade=1')
+	animatedcollapse.addDiv('2documentname22', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname23', 'fade=1')
+	animatedcollapse.addDiv('2documentname23', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname24', 'fade=1')
+	animatedcollapse.addDiv('2documentname24', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname25', 'fade=1')
+	animatedcollapse.addDiv('2documentname25', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname26', 'fade=1')
+	animatedcollapse.addDiv('2documentname26', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname27', 'fade=1')
+	animatedcollapse.addDiv('2documentname27', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname28', 'fade=1')
+	animatedcollapse.addDiv('2documentname28', 'fade=1,hide=1')
+
+	animatedcollapse.addDiv('1documentname29', 'fade=1')
+	animatedcollapse.addDiv('2documentname29', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname30', 'fade=1')
+	animatedcollapse.addDiv('2documentname30', 'fade=1,hide=1')	
+	
+	animatedcollapse.addDiv('1documentname31', 'fade=1')
+	animatedcollapse.addDiv('2documentname31', 'fade=1,hide=1')	
+
+	animatedcollapse.addDiv('1documentname32', 'fade=1')
+	animatedcollapse.addDiv('2documentname32', 'fade=1,hide=1')	
+
+	animatedcollapse.addDiv('1documentname33', 'fade=1')
+	animatedcollapse.addDiv('2documentname33', 'fade=1,hide=1')	
+
+	animatedcollapse.addDiv('1documentname34', 'fade=1')
+	animatedcollapse.addDiv('2documentname34', 'fade=1,hide=1')	
+
+	animatedcollapse.addDiv('1documentname35', 'fade=1')
+	animatedcollapse.addDiv('2documentname35', 'fade=1,hide=1')	
+	
+	//form section divs for page division
+		//main section that break the page into seperate divs
+		animatedcollapse.addDiv('formsection1', 'speed=50,fade=1')
+		animatedcollapse.addDiv('formsection2', 'speed=50,fade=1,hide=1')
+		animatedcollapse.addDiv('formsection3', 'speed=50,fade=1,hide=1')
+		animatedcollapse.addDiv('formsection4', 'speed=50,fade=1,hide=1')
+		
+		//sections that hide the extra input areas (e.g. references boxes 3 and 4)
+		animatedcollapse.addDiv('subformsection1', 'fade=1,hide=1')
+		
+		//bottom buttons
+		animatedcollapse.addDiv('formsectionbutton1', 'fade=1')
+		animatedcollapse.addDiv('formsectionbutton2', 'fade=1,hide=1')
+		animatedcollapse.addDiv('formsectionbutton3', 'fade=1,hide=1')
+		animatedcollapse.addDiv('formsectionbutton4', 'fade=1')
+		
+		//static menu buttons
+		animatedcollapse.addDiv('staticbutton1', 'fade=1')
+		animatedcollapse.addDiv('staticbutton2', 'fade=1,hide=1')
+		animatedcollapse.addDiv('staticbutton3', 'fade=1,hide=1')
+		animatedcollapse.addDiv('staticbutton4', 'fade=1')
+		
+		animatedcollapse.addDiv('contactsection', 'fade=1')
+		
+		animatedcollapse.addDiv('promo_bookmarks', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_posters', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_flyers', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_social', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_social_seasonal', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_banners', 'fade=1,hide=1')
+		animatedcollapse.addDiv('promo_buttons', 'fade=1,hide=1')	
+
+		animatedcollapse.addDiv('more_info', 'fade=1,hide=1')			
+		
+	
+	/*init must be on bottom!!!*/
+	animatedcollapse.init()
+	
